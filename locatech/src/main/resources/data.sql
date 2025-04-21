261dbe74e72e45ed27362e909663f06af99c74c0
carlospaulinodevjava <carlospaulinodevjava@gmail.com>
2025-04-21 15:25:38 -0300

    FASE I - Aula 4 - Video 5 - incluindo CRUD de Pessoa.
    FASE I - Aula 4 - Video 5 - incluindo CRUD de Pessoa.

--- locatech/src/main/resources/data.sql
+++ locatech/src/main/resources/data.sql
@@ -8,5 +8,16 @@ CREATE TABLE VEICULOS(
   valor_diaria DECIMAL(10,2)
 );
 
+CREATE TABLE PESSOAS(
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(255),
+  cpf VARCHAR(255),
+  telefone VARCHAR(255),
+  email VARCHAR(255)
+);
+
 INSERT INTO veiculos(marca, modelo, placa, ano, cor, valor_diaria) VALUES
-    ('Chevrolet','Celta', 'ABC-1234', 2010, 'preta', 100.00)
+    ('Chevrolet','Celta', 'ABC-1234', 2010, 'preta', 100.00);
+
+INSERT INTO PESSOAS(nome, cpf, telefone, email) VALUES
+    ('Carlos','99999999999-99', '11-99999999', 'carlos@fiap.com.br');
