f62e1e7b7085648541350984f3bcd63fe04de4e0
carlospaulinodevjava <carlospaulinodevjava@gmail.com>
2025-04-21 12:11:05 -0300

    FASE I - Aula 4 - Video 3. injetando repository no VeiculoService
    FASE I - Aula 4 - Video 3. injetando repository no VeiculoService

--- locatech/src/main/java/br/com/fiap/locatech/locatech/controllers/VeiculoController.java
+++ locatech/src/main/java/br/com/fiap/locatech/locatech/controllers/VeiculoController.java
@@ -1,16 +1,43 @@
 package br.com.fiap.locatech.locatech.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.fiap.locatech.locatech.entities.Veiculo;
+import br.com.fiap.locatech.locatech.services.VeiculoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/veiculo")
 public class VeiculoController {
 
+    private static final  Logger logger = LoggerFactory.getLogger(VeiculoController.class);
+
+    private final VeiculoService veiculoService;
+
+    public VeiculoController(VeiculoService veiculoService){
+        this.veiculoService = veiculoService;
+    }
+
     @GetMapping
-    public String veiculos(){
-        return "Hello Veiculos!!";
+    public ResponseEntity<List<Veiculo>> findAllVeiculos(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size){
+        logger.info("Foi acesso o endpoint de veiculos /veiculos");
+        var veiculos = this.veiculoService.findAllVeiculos(page, size);
+        return ResponseEntity.ok(veiculos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Veiculo>> findVeiculo(
+            @PathVariable("id") Long id
+    ){
+        logger.info("/veiculos/" + id);
+        var veiculo = this.veiculoService.findVeiculoById(id);
+        return ResponseEntity.ok(veiculo);
     }
 
 }
