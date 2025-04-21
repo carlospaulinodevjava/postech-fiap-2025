a7fc1502f04e34803eef4d8968dca5cc1d3196b0
carlospaulinodevjava <carlospaulinodevjava@gmail.com>
2025-04-21 18:23:41 -0300

    FASE I - Aula 4 - Video 7 - Incluindo ControllerExceptionHandler
    FASE I - Aula 4 - Video 7 - Incluindo ControllerExceptionHandler

--- locatech/src/main/java/br/com/fiap/locatech/locatech/services/AluguelService.java
+++ locatech/src/main/java/br/com/fiap/locatech/locatech/services/AluguelService.java
@@ -4,6 +4,7 @@ import br.com.fiap.locatech.locatech.dtos.AluguelRequestDTO;
 import br.com.fiap.locatech.locatech.entities.Aluguel;
 import br.com.fiap.locatech.locatech.repositories.AluguelRepository;
 import br.com.fiap.locatech.locatech.repositories.VeiculoRepository;
+import br.com.fiap.locatech.locatech.services.exceptions.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
@@ -28,7 +29,7 @@ public class AluguelService {
     }
 
     public Optional<Aluguel> findAlugueisById(Long id) {
-        return this.aluguelRepository.findById(id);
+        return Optional.ofNullable(this.aluguelRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Aluguel não encontratdo")));
     }
 
     public void saveAluguel(AluguelRequestDTO aluguel) {
