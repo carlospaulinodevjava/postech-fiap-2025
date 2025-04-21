f5450a02d1b065dcbdff378f00def00e385ea3e7
carlospaulinodevjava <carlospaulinodevjava@gmail.com>
2025-04-21 16:14:11 -0300

    FASE I - Aula 4 - Video 6 - incluindo CRUD de  Alugueis.
    FASE I - Aula 4 - Video 6 - incluindo CRUD de  Alugueis.

---
+++ locatech/src/main/java/br/com/fiap/locatech/locatech/services/AluguelService.java
@@ -0,0 +1,50 @@
+package br.com.fiap.locatech.locatech.services;
+
+import br.com.fiap.locatech.locatech.entities.Aluguel;
+import br.com.fiap.locatech.locatech.repositories.AluguelRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AluguelService {
+
+    private final AluguelRepository aluguelRepository;
+
+    public AluguelService(AluguelRepository aluguelRepository) {
+        this.aluguelRepository = aluguelRepository;
+    }
+
+    public List<Aluguel> findAllAlugueis(int page, int size) {
+        int offset = (page - 1) * size;
+        return this.aluguelRepository.findAll(size, offset);
+    }
+
+    public Optional<Aluguel> findAlugueisById(Long id) {
+        return this.aluguelRepository.findById(id);
+    }
+
+    public void saveAluguel(Aluguel aluguel) {
+        var save = aluguelRepository.save(aluguel);
+        Assert.state(save == 1, "Erro ao salvar aluguel" + aluguel.getPessoaNome());
+    }
+
+    public void updateAluguel(Aluguel aluguel, Long id) {
+        var update = aluguelRepository.update(aluguel, id);
+
+        if (update == 0) {
+            throw new RuntimeException("Aluguel não encontrado");
+        }
+    }
+
+    public void deleteAluguel(Long id) {
+        var delete = aluguelRepository.delete(id);
+
+        if (delete == 0) {
+            throw new RuntimeException("Aluguel não encontrado");
+        }
+    }
+
+}
--- locatech/src/main/resources/data.sql
+++ locatech/src/main/resources/data.sql
@@ -16,8 +16,23 @@ CREATE TABLE PESSOAS(
   email VARCHAR(255)
 );
 
+CREATE TABLE alugueis (
+     id BIGINT AUTO_INCREMENT PRIMARY KEY,
+     pessoa_id BIGINT NOT NULL,
+     veiculo_id BIGINT NOT NULL,
+     data_inicio DATE,
+     data_fim DATE,
+     valor_total DECIMAL(10, 2),
+     FOREIGN KEY (pessoa_id) REFERENCES pessoas(id),
+     FOREIGN KEY (veiculo_id) REFERENCES veiculos(id)
+);
+
 INSERT INTO veiculos(marca, modelo, placa, ano, cor, valor_diaria) VALUES
     ('Chevrolet','Celta', 'ABC-1234', 2010, 'preta', 100.00);
 
 INSERT INTO PESSOAS(nome, cpf, telefone, email) VALUES
     ('Carlos','99999999999-99', '11-99999999', 'carlos@fiap.com.br');
+
+
+INSERT INTO alugueis (pessoa_id, veiculo_id, data_inicio, data_fim, valor_total) VALUES
+    (1, 1, '2025-04-15', '2025-04-30', 1500.00);
