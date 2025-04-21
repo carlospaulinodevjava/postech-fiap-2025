2f0d71c46deea765b6985cc17b7c3c9586a979f5
carlospaulinodevjava <carlospaulinodevjava@gmail.com>
2025-04-21 13:51:28 -0300

    FASE I - Aula 4 - Video 5 - ajuste URI get all veiculos
    FASE I - Aula 4 - Video 5 - ajuste URI get all veiculos

--- locatech/src/main/java/br/com/fiap/locatech/locatech/controllers/VeiculoController.java
+++ locatech/src/main/java/br/com/fiap/locatech/locatech/controllers/VeiculoController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/veiculo")
+@RequestMapping("/veiculos")
 public class VeiculoController {
 
     private static final  Logger logger = LoggerFactory.getLogger(VeiculoController.class);
